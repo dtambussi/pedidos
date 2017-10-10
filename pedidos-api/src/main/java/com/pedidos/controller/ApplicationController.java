ba13d4d42997c1245c51690399953c06db6d2631
favio <favioemiliano@gmail.com>
2017-10-10 11:37:11 -0300

    Agregado request body2

--- pedidos-api/src/main/java/com/pedidos/controller/ApplicationController.java
+++ pedidos-api/src/main/java/com/pedidos/controller/ApplicationController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -80,7 +81,7 @@ public class ApplicationController {
 
 	@ResponseStatus(HttpStatus.OK)
 	@PostMapping("/pedidos")
-	public Pedido generarPedido(final GenerarPedidoRequest generarPedidoRequest) {
+	public Pedido generarPedido(@RequestBody final GenerarPedidoRequest generarPedidoRequest) {
 		return this.pedidoService.generarPedido(generarPedidoRequest);
 	}
 	
