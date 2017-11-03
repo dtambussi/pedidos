3301390422216b05c71a90813679b5583fda6650
damian.tambussi <dtambussi@educatina.com>
2017-11-03 17:19:30 -0300

    Advances

--- pedidos-ui/web/app.php
+++ pedidos-ui/web/app.php
@@ -6,7 +6,7 @@ require __DIR__.'/../vendor/autoload.php';
 if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
 }
-
+date_default_timezone_set('America/Argentina/Buenos_Aires');
 $kernel = new AppKernel('dev', true);
 if (PHP_VERSION_ID < 70000) {
     $kernel->loadClassCache();
